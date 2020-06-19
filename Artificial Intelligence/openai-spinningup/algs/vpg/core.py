be174c8cb936a832d67b9ce32f32f1f39d493288
THCtw <everybody3463@gmail.com>
2020-06-19 15:58:14 +0800

    Add VPG

--- Artificial Intelligence/openai-spinningup/algs/vpg/core.py
+++ Artificial Intelligence/openai-spinningup/algs/vpg/core.py
@@ -33,18 +33,52 @@ class PGNet(nn.Module):
         hid = torch.tanh(self.fc2(hid))
         return F.softmax(self.fc3(hid), dim=-1)
 
-class VPGAgent():
+class VFNet(nn.Module):
 
-    def __init__(self, network):
-        self.network = network
-        self.optimizer = optim.SGD(self.network.parameters(), lr=0.001)
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(8, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, 4)
+        self.fc4 = nn.Linear(8, 1)
 
-    def learn(self, log_probs, rewards, EPISODE_PER_BATCH):
-        loss = (-log_probs * rewards).sum() / EPISODE_PER_BATCH
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+    def forward(self, state):
+        hid = torch.tanh(self.fc1(state))
+        hid = torch.tanh(self.fc2(hid))
+        hid = torch.tanh(self.fc3(hid))
+        return self.fc4(hid)
     
+class PGAgent():
+
+	def __init__(self, pgnet, vfnet):
+        self.pgnet = pgnet
+        self.vfnet = vfnet
+        self.pg_optimizer = optim.SGD(self.pgnet.parameters(), lr=0.001)
+        self.vf_optimizer = optim.SGD(self.vfnet.parameters(), lr=0.001)
+
+    def update_pgnet(self, log_probs, states, episodes_per_batch):
+        advs = self.vfnet(torch.FloatTensor(states))
+
+        log_probs = torch.stack(log_probs)
+        advs = torch.stack(advs)
+        loss = (-log_probs * advs).mean()
+
+        self.pg_optimizer.zero_grad()
+        loss.backward()
+        self.pg_optimizer.step()
+
+    def update_vfnet(self, states, ret):
+        vals = self.vfnet(torch.FloatTensor(states)
+        vals = torch.stack(vals)
+        loss = ((vals - ret)**2).mean()
+        
+        self.vf_optimizer.zero_grad()
+        loss.backward()
+        self.vf_optimizer.step()
+
     def sample(self, state):
-        action_prob = self.network(torch.FloatTensor(state))
+        action_prob = self.pgnet(torch.FloatTensor(state))
+        action_dist = Categorical(action_prob)
+        action = action_dist.sample()
+        log_prob = action_dist.log_prob(action)
+        return action.item(), log_prob
--- Artificial Intelligence/openai-spinningup/algs/vpg/vpg.py
+++ Artificial Intelligence/openai-spinningup/algs/vpg/vpg.py
@@ -4,10 +4,85 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
+import tqdm
+import matplotlib.pyplot as plt
+
+import gym
 
 import algs.vpg.core as core
 
-def vpg():
+def vpg(env, record=1, policyAgent=core.PGAgent, gamma=0.5, seed=0, episodes_per_batch=5, num_batch=400):
+    
+    # Set up OpenAI Gym environment
+    env = env()
+
+    # Set up policy agent
+    pgnet = core.PGNet()
+    vfnet = core.VFNet
+    agent = policyAgent(pgnet, vfnet)
+
+    agent.pgnet.train()
+    agent.vfnet.train()
+
+    avg_total_rewards, avg_final_rewards = [], []
+    
+    prg_bar = tqdm(range(num_batch))
+    for batch in prg_bar:
+
+        states, log_probs, rewards, ret = [], [], [], []
+        total_rewards, final_rewards = [], []
+
+        # Collecting training data
+        for episode in range(episodes_per_batch):
+
+            state = env.reset()
+            total_reward, total_step = 0, 0
+
+            episode_rewards = []
+            episode_states = []
+
+            while True:
+
+                action, log_prob = agent.sample(state)
+                next_state, reward, done, _ = env.step(action)
+
+                log_probs.append(log_prob)
+                episode_states.append(state)
+                episode_rewards.append(reward)
+
+                state = next_state
+                total_reward += reward
+                total_step += 1
+
+                if done:
+
+                    final_rewards.append(reward)
+                    total_rewards.append(total_reward)
+                    states.append(episode_states)
+                    rewards.append(episode_rewards)
+                    ret.append(core.discount_cumsum(rewards, gamma)
+
+                    break
+    	# Log training process
+        avg_total_reward = sum(total_rewards) / len(total_rewards)
+        avg_final_reward = sum(final_rewards) / len(final_rewards)
+        avg_total_rewards.append(avg_total_reward)
+        avg_final_rewards.append(avg_final_reward)
+        prg_bar.set_description(f"Total: {avg_total_reward: 4.1f}, Final: {avg_final_reward: 4.1f}")    
+
+        # Update Policy Gradient Network
+        agent.update_pgnet(log_probs, states, episodes_per_batch)
+        ret = np.concatenate(ret, axis=0)
+        agent.update_vfnet(states, torch.from_numpy(ret)) 
+
+        plt.plot(avg_total_rewards)
+        plt.title("Total Rewards")
+        plt.savefig('avg_total_rewards.png')
+
+        plt.plot(avg_final_rewards)
+        plt.title("Final Rewards")
+        plt.savefig('avg_final_rewards.png')
+
     return True
 
 
@@ -18,9 +93,9 @@ if __name__ == '__main__':
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--cpu', type=int, default=4)
-    parser.add_argument('--steps', type=int, default=4000)
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--episodes', type=int, default=4000)
+    parser.add_argument('--batches', type=int, default=50)
     parser.add_argument('--exp_name', type=str, default='vpg')
     args = parser.parse_args()
 
-    vpg(lambda : gym.make(args.env), policyAgent=core.VPGAgent, gamma=args.gamma, seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs) 
+    vpg(lambda : gym.make(args.env), policyAgent=core.VPGAgent, gamma=args.gamma, seed=args.seed, episodes_per_batch=args.episodes, num_batch=batches) 
